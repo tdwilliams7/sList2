5be1b0b76c8bc18cc2441bb79ececc46fd90425a
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-10-19 15:37:47 -0500

    fixed diplay problem innerHtml = '' at beginning of the function. added alerts and prevented added items without price or name.

--- js/script.js
+++ js/script.js
@@ -20,16 +20,17 @@ var items = [
 let list = document.getElementById('list');
 
 function loadList(arr){
-
+  list.innerHTML = '';
   for (var i = 0; i < arr.length; i++){
     let newItem = document.createElement('div');
     newItem.setAttribute('class', 'row');
     newItem.innerHTML = `<div class='col-xs-4'>` + arr[i].name + `</div>
     <div class='col-xs-4'>`+ '$ ' + arr[i].price + `</div>
     <div class='col-xs-2'><input type='checkbox'></input></div>
-    <button><i class='fa fa-trash' onClick=deleteFoodButton(this)></i></button>`;
+    <button onClick=deleteFoodButton(this)><i class='fa fa-trash'></i></button>`;
     list.appendChild(newItem);
   }
+
 
 }
 loadList(items);
@@ -42,31 +43,43 @@ function getTotal(arr){
   }
   console.log(total)
   let totalDiv = document.getElementById('total');
-  totalDiv.innerHTML = `<h4>`+total+`</h4>`;
+  totalDiv.innerHTML = `<h4>`+total.toFixed(2)+`</h4>`;
 }
 getTotal(items);
 
 
 function deleteFoodButton(e){
   console.log('delete');
-  e.parentNode.parentNode.remove(e.id)
+  e.parentNode.remove(e.id)
 
 };
 
 let addFoodButton = document.getElementById('addFoodButton');
+let foodAlert = document.getElementById('foodAlert');
+let priceAlert = document.getElementById('priceAlert');
 
 addFoodButton.addEventListener('click', function(){
-  let foodName = document.getElementById('foodName').value;
-  let foodPrice = document.getElementById('foodPrice').value;
-  var newItemObj = {
-    name: foodName,
-    price: (parseFloat(foodPrice)),
+  let foodName = document.getElementById('foodName');
+  let foodPrice = document.getElementById('foodPrice');
+  if (foodName.value.length > 0 && foodPrice.value.length > 0){
+    var newItemObj = {
+    name: foodName.value,
+    price: (parseFloat(foodPrice.value)),
     completed: false
   }
   items.push(newItemObj);
   console.log(items);
   loadList(items);
   getTotal(items);
+  foodName.value = '';
+  foodPrice.value= '';
+  foodAlert.textContent = '';
+  priceAlert.textContent = '';
+  } else {
+    foodAlert.textContent ='please enter a food';
+    priceAlert.textContent = 'Please enter a Price';
+  }
+
 })
 
 // function inputNewItem() {
